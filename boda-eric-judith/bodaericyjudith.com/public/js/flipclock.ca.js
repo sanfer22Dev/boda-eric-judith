d62db95273b4559934cdd26c4a34f67e84af6fab
sanfer22Dev <146326036+sanfer22Dev@users.noreply.github.com>
2025-06-18 12:22:02 +0200

    Add files via upload

---
+++ boda-eric-judith/bodaericyjudith.com/public/js/flipclock.ca.js
@@ -0,0 +1,12 @@
+(function () {
+  FlipClock.Lang.Catalan = {
+    years: "Anys",
+    months: "Mesos",
+    days: "Dies",
+    hours: "Hores",
+    minutes: "Minuts",
+    seconds: "Segons",
+  };
+
+  FlipClock.Lang["ca"] = FlipClock.Lang.Catalan;
+})();
